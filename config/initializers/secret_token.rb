fe9938100854baeabef1ce756b6ef719baa2e2ec
Brad Barrows <bbarrows@bittorrent.com>
2012-07-09 15:16:59 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utorrenttoolboxserver::Application.config.secret_token = '75450b8aa63c1e45df4a01817195856edb15f35a358ba65090a971d89c884d6a798152e7c5c2ec35b6e8513b9981d96c80a0213d0621da1da9c3a3e6c47dc0d3'
